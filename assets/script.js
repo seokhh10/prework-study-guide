0ec1f45952c4d0f35dd4478303bfc5ea2cfefab1
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-03-06 13:49:49 -0600

    declared variables using const, use let, assing operator=. I used array to store multiple data items in a variable. I used for loop to iterate over the array so that  the block of code will be iterated over. 4 topics are logged in the console separately.

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(let HTML = 0; HTML < topics.length; HTML++) {
+console.log(topics[HTML]);
+}
+
+/*
+if (topic = 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic = 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic = 'Git') {
+  console.log("Let's study Git!");
+} else if (topic = 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
